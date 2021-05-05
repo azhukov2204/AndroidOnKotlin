3414ac869573c54da404889c1e09174b2c21ab60
azhukov2204 <andrew284@mail.ri>
2021-05-05 18:46:37 +0500

    after code review: the static value is moved to a constant

--- app/src/main/java/ru/androidlearning/moviesearch/viewmodel/MovieDetailViewModel.kt
+++ app/src/main/java/ru/androidlearning/moviesearch/viewmodel/MovieDetailViewModel.kt
@@ -7,6 +7,8 @@ import ru.androidlearning.moviesearch.model.MovieRepositoryImpl
 import java.lang.Thread.sleep
 import java.util.*
 
+const val sleepTimeInMills: Long = 1000
+
 class MovieDetailViewModel(
     private val moveDetailsLiveData: MutableLiveData<AppState> = MutableLiveData(),
     private val movieRepository: MovieRepository = MovieRepositoryImpl()
@@ -17,7 +19,7 @@ class MovieDetailViewModel(
     fun getMovieDetailsFromLocalSource() {
         moveDetailsLiveData.value = AppState.Loading
         Thread {
-            sleep(1000)
+            sleep(sleepTimeInMills)
             val appState =
                 if (random.nextInt(2) == 0) AppState.Success(movieRepository.getMovieDetailsFromLocalStorage())
                 else AppState.Error(Throwable("Error of detail load"))
